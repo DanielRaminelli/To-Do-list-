0203197f9bcb15cb5f2cd75e3d16c793f91afeb2
DanielRaminelli <danielraminelli@hotmai.com>
2020-09-30 10:56:16 -0300

    Adicionar a criação de novas tarefas

--- todolist-rd/js/script.js
+++ todolist-rd/js/script.js
@@ -1,9 +1,22 @@
 console.log('meu nome é Daniel')
 
-let botaoAlerta = document.querySelector('#adicionar-tarefa')
-let inputTarefa = document.querySelector('#tarefa-digitada')
+let botaoAlerta = document.querySelector('#adicionar-tarefa');
+let inputTarefa = document.querySelector('#tarefa-digitada');
+let divTarefas=document.querySelector('#tarefas')
 
 function criarTarefa(){
+    let tarefa = `
+    <div class="col-md-4">
+        <div class="card-tarefa">
+            <div class="texto-tarefa">
+                ${inputTarefa.value}
+            </div>
+            <div class="botao-tarefa">
+                <img src="img/check.png" width="32" alt="Botão para finalizar tarefa" title="Botão para finalizar tarefa">
+            </div>
+        </div>
+    </div>
+    `
     alert('Tarefa Criada: ' + inputTarefa.value)
 }
 
